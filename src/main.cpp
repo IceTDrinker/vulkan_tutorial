6efb44598b7fcdbd1060603234fe1afb5c49aa27
IceTDrinker <49040125+IceTDrinker@users.noreply.github.com>
2020-04-13 12:29:42 +0200

    Vulkan Basic init tutorial https://vulkan-tutorial.com/en/Drawing_a_triangle/Setup/Instance
    - Update CMakeLists to tidy up some sections
    - Updated main.cpp for tutorial

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,14 @@
 class HelloTriangleApplication
 {
 public:
+    const uint32_t WIDTH = 800;
+    const uint32_t HEIGHT = 600;
+
+private:
+    VkInstance instance = nullptr;
+    GLFWwindow* window = nullptr;
+
+public:
     void run()
     {
         initWindow();
@@ -16,9 +24,6 @@ public:
         mainLoop();
         cleanup();
     }
-
-    const uint32_t WIDTH = 800;
-    const uint32_t HEIGHT = 600;
 
 private:
     void initWindow()
@@ -31,9 +36,39 @@ private:
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
 
+    void createInstance()
+    {
+        VkApplicationInfo appInfo = {};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo = {};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        createInfo.enabledLayerCount = 0;
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+        {
+            throw std::runtime_error("failed to create instance!");
+        }
+    }
+
     void initVulkan()
     {
-
+        createInstance();
     }
 
     void mainLoop()
@@ -46,12 +81,12 @@ private:
 
     void cleanup()
     {
+        vkDestroyInstance(instance, nullptr);
+
         glfwDestroyWindow(window);
 
         glfwTerminate();
     }
-
-    GLFWwindow* window;
 };
 
 int main()
